e3834c6994d5999a357008e1236f67c24cff0f0e
Jonathan Young <YoungJon@uw.edu>
2021-07-26 20:10:17 -0700

    Upload initial scripts with minimal testing

---
+++ README.md
@@ -0,0 +1,3 @@
+#Micro-AV build Scripts
+
+The scripts here are intended to build a working base environment for the purposes of building and testing the MuSHR car with the necessary dependencies installed.
---
+++ microav_mavros_install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#This script is intended for use with the Micro-AV MuSHR car. Written and maintained by Jonathan Young.
+
+# Install MAVROS and extras (https://github.com/mavlink/mavros/blob/master/mavros/README.md#binary-installation-deb) 
+sudo apt-get install ros-melodic-mavros ros-melodic-mavros-extras
+
+wget https://raw.githubusercontent.com/mavlink/mavros/master/mavros/scripts/install_geographiclib_datasets.sh
+
+#	 for an executable:
+# chmod +x install_geographiclib_datasets.sh
+# 	or for manual install
+# sudo bash install_geographiclib_datasets.sh
+source install_geographiclib_datasets.sh
+
+# install the python catkin package manager
+pip3 install python-catkin_pkg
+
+# make catkin workspace directory and move into it
+mkdir -p ~/catkin_ws/src && cd ~/catkin_ws/
+
+# build the workspace
+catkin_make -DPYTHON_EXECUTABLE=/usr/bin/python3
+
+# source the workspace so that every bash profile can find it
+echo “source ./devel/setup/bash” >> ~/.bashrc
