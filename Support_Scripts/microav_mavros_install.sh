27e8a7a4b497964bc5ed9619dd6774350a6d298f
Jonathan Young <YoungJon@uw.edu>
2021-08-06 15:38:38 -0700

    removed source to bashrc; added python3-empy to apt-get install; added rospkg to pip3 install

--- Support_Scripts/microav_mavros_install.sh
+++ Support_Scripts/microav_mavros_install.sh
@@ -3,7 +3,7 @@
 #This script is intended for use with the Micro-AV MuSHR car. Written and maintained by Jonathan Young.
 
 # Install MAVROS and extras (https://github.com/mavlink/mavros/blob/master/mavros/README.md#binary-installation-deb) 
-sudo apt-get install ros-melodic-mavros ros-melodic-mavros-extras -y
+sudo apt-get install ros-melodic-mavros ros-melodic-mavros-extras python3-empy -y
 
 wget https://raw.githubusercontent.com/mavlink/mavros/master/mavros/scripts/install_geographiclib_datasets.sh
 
@@ -14,7 +14,7 @@ sudo bash install_geographiclib_datasets.sh
 
 # install the python catkin package manager
 # Do not use the apt installer- this will uninstall all python2 catkin tools
-sudo -H pip3 install catkin-pkg
+sudo -H pip3 install catkin-pkg rospkg
 
 rm install_geographiclib_datasets.sh
 
@@ -24,5 +24,5 @@ mkdir -p ~/catkin_ws/src && cd ~/catkin_ws/
 # build the workspace
 catkin_make -DPYTHON_EXECUTABLE=/usr/bin/python3
 
-# source the workspace so that every bash profile can find it
-echo “source ./devel/setup/bash” >> ~/.bashrc
+# source the workspace so that every bash profile can find it --- not needed as this is done in "microav_mush_stack.sh"
+# echo “source ./devel/setup.bash” >> ~/.bashrc
